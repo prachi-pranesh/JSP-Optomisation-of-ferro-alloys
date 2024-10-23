9d3d91e2a912e980c92b3b290bec5da1c16fc72f
PRACHI PRANESH <135634283+prachi-pranesh@users.noreply.github.com>
2024-10-24 03:07:55 +0530

    Update main.py

--- JSPL Internship project_model ml/main.py
+++ JSPL Internship project_model ml/main.py
@@ -1,27 +1,43 @@
-import pandas as pd
 import numpy as np
-from RecoveryCalculator import RecoveryCalculator
-from CalculateAlloys import CalculateAlloys
-from tensorflow.keras.optimizers import Adam 
+import pandas as pd
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression 
+from utils import load_and_preprocess_data
 
 df = pd.read_excel(r"data_sheet.xlsx",sheet_name=1)   
-obj=RecoveryCalculator(df)
-df1=pd.read_excel(r"data_sheet.xlsx",sheet_name='INPUT') 
-x,y=obj.load_and_preprocess_data(df1)
-obj.model.compile(optimizer='Adam',loss='MSE',metrics='accuracy')
-hist=obj.model.fit(x=x,y=y,batch_size=33,epochs=25) #
-print("Training of Recovery Calculator model completed. Starting second phase of taining")      
-calculator=CalculateAlloys(obj.model)
-calculator.compile(Adam(learning_rate=0.0001))
-calculator.fit(y,batch_size=1,epochs=100)
-Mn=0.75
-C=0.1
-Si=0.08
-S=0.021
-Nb=0
-Al=0.025
-test=[Mn,C,Si,S,Nb,Al]
-test=np.array(test,dtype=np.float32) 
-output=calculator.predict(np.expand_dims(test,axis=0))
-print(f"SiMn: {1000*output[0,0]} \n HCFeMn: {1000*output[0,1]}\n FeSi: {1000*output[0,2]}\n FeNb: {1000*output[0,3]}\n CPC: {1000*output[0,4]} \n Lime: {1000*output[0,5]} \n Al: {1000*output[0,6]}")
-calculator.save(r"C:\Users\anura\OneDrive\Desktop\clutter file (1)\Prach_Pranesh_JSPL_project")
+y,x=load_and_preprocess_data(df)
+
+def Linearfit(X,Y):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.0625)
+    regr = LinearRegression()   
+    regr.fit(X_train, y_train) 
+    return regr
+
+SiMn_pred=Linearfit(x,y[:,0])
+HCFeMn_pred=Linearfit(x,y[:,1])
+FeSi_pred=Linearfit(x,y[:,2])
+FeNb_pred=Linearfit(x,y[:,3])
+CPC_pred=Linearfit(x,y[:,4])
+Lime_pred=Linearfit(x,y[:,5])
+Al_pred=Linearfit(x,y[:,6])
+
+def calculate_alloys(Mn0,C0,Si0,S0,Nb0,Al0):
+    Mn=1.3-Mn0
+    C=0.17-C0
+    Si=0.17-Si0
+    S=0.003-S0
+    Nb=0.02-Nb0
+    Al=0.03-Al0
+    test=[Mn,C,Si,S,Nb,Al]
+    test=np.array(test,dtype=np.float32)
+    x_t=np.expand_dims(test,axis=0)
+    SiMn=SiMn_pred.predict(x_t)
+    HCFeMn=HCFeMn_pred.predict(x_t)
+    FeSi=FeSi_pred.predict(x_t)
+    FeNb=FeNb_pred.predict(x_t)
+    CPC=CPC_pred.predict(x_t)
+    Lime=Lime_pred.predict(x_t)
+    Al=Al_pred.predict(x_t)
+    print(f"SiMn: {1000*SiMn} \n HCFeMn: {1000*HCFeMn}\n FeSi: {1000*FeSi}\n FeNb: {1000*FeNb}\n CPC: {1000*CPC} \n Lime: {1000*Lime} \n Al wire: {1000*Al}")
+print("Example:")  
+calculate_alloys(0.75,0.1,0.08,0.021,0,0.025)
